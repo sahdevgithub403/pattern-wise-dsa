91df882d0714407a91ba1cfa88bef76b9ce743ed
sahdevgithub403 <sahdeva403@gmail.com>
2026-01-20 17:16:26 +0530

    add one more problem of string

--- String/IsSubsequence.java
+++ String/IsSubsequence.java
@@ -1,7 +1,26 @@
 public class IsSubsequence {
 
-    
-    public static void main(String[] args) {
-        
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        int s1 = s.length();
+        int s2 = t.length();
+
+        while (i < s1 && j < s2) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            } 
+            j++;  
+        }
+        return i == s.length();
+    }
+
+    public static void main(String[] sahdev) {
+        String s = "abc";
+        String t = "ahgdc";
+        boolean res = isSubsequence(s, t);
+        System.out.println(res);
+
     }
 }
