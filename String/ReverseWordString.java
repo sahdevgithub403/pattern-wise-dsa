4361fd6293fc9dc58113833bf7849cae25ac6732
sahdevgithub403 <sahdeva403@gmail.com>
2026-01-20 16:24:36 +0530

    add one more problem of string

---
+++ String/ReverseWordString.java
@@ -0,0 +1,21 @@
+public class ReverseWordString {
+
+    public static String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        String res = reverseWords(s);
+        System.out.println(res);
+    }
+}
+
